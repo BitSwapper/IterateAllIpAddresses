e9b5fb92f624a2610d19f4fe8bbe6b34980716de
Hex <119023047+SleepyHex@users.noreply.github.com>
2022-12-17 17:40:54 -0500

    Update Octets.cs

--- Octets.cs
+++ Octets.cs
@@ -3,7 +3,7 @@ long MaxCt = (long)256 * 256 * 256 * 256;
             Console.WriteLine("{0}.{1}.{2}.{3}", i % 256, (i / 256) % 256, (i / (256 * 256)) % 256, (i / (256 * 256 * 256)) % 256);
 
 //The modulus operator (%) is used to extract the octets of the IP address in this code by taking the remainder of i divided by a power of 256.
-//For example, the expression i % 256 extracts the lowest octet of the IP address by taking the remainder of i divided by 256. 
+//the expression i % 256 extracts the lowest octet of the IP address 
 //If i is a 32-bit integer with a value in the range 0-255, then i % 256 will simply return the value of i. 
-//If i is a larger value, then i % 256 will return the remainder of i divided by 256, which will be a value in the range 0-255.
+//If i is a larger than the size of a single byte, i % 256 will return the remainder of i divided by 256, which will be a value in the range 0-255.
 //There is a lot of division taking place here, which is quite expensive. Hence forthe the final example, OctetsWithBitShifting
