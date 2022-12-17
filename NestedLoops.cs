ce7c509fed47aff61087c17b4a0c9ab9dc9c8124
Hex <119023047+SleepyHex@users.noreply.github.com>
2022-12-17 17:24:35 -0500

    Update NestedLoops.cs

--- NestedLoops.cs
+++ NestedLoops.cs
@@ -3,3 +3,7 @@ for (int i = 0; i <= 255; i++)
         for (int k = 0; k <= 255; k++)
             for (int l = 0; l <= 255; l++)
                 Console.WriteLine($"{i}.{j}.{k}.{l}");
+
+
+//this is what i would call the "traditional", or intuitive way of solving this task.
+//Not as fun, or fancy as the other examples though.
